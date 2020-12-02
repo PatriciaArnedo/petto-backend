294a3ccf7e85774cbdfab8d958ebcc06c2726666
aru120 <khuang159@gmail.com>
2020-12-02 18:41:12 -0500

    added user create

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -10,6 +10,17 @@ class Api::UsersController < ApplicationController
         render json: user
     end
 
+    def create
+        user = User.create(user_params)
+        render json: user     
+    end
+
+private
+
+    def user_params
+        params.permit(:name)
+    end
+
 
 end
 
