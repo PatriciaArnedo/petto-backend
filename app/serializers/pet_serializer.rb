dae65a3c9293def6a614b29cada6c9141164ab5b
PatriciaArnedo <paaarnedo@gmail.com>
2020-12-04 00:01:16 -0500

    edited pet serializer

--- app/serializers/pet_serializer.rb
+++ app/serializers/pet_serializer.rb
@@ -1,4 +1,5 @@
 class PetSerializer < ActiveModel::Serializer
-  attributes :id, :name, :hunger, :cleanliness, :energy, :happiness, :happy_img, :sad_img, :user_id
+  attributes :id, :name, :hunger, :cleanliness, :energy, :happiness, :happy_img, :sad_img, :user_id, :bio, :created_at
+  belongs_to :user
   
 end
